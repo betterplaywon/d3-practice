35f84449394560f0fcc7714f846f588fa66ccd68
강지원 <kkgi1234@gmail.com>
2026-03-03 20:05:04 +0900

    fix: 전역 App에 그래프 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,54 @@
 import { Routes, Route, Link } from "react-router-dom";
-import Lesson01 from "./components/lesson/lesson-01";
-import Lesson02 from "./components/lesson/lesson-02";
-import Lesson03 from "./components/lesson/lesson-03";
-import Lesson04 from "./components/lesson/lesson-04";
-import Lesson05 from "./components/lesson/lesson-05";
-import Lesson06 from "./components/lesson/lesson-06";
-import Lesson07 from "./components/lesson/lesson-07";
+import SvgBasics from "./components/lesson/svg-basics";
+import D3DomManipulation from "./components/lesson/d3-dom-manipulation";
+import ScatterPlot from "./components/lesson/scatter-plot";
+import BarChart from "./components/lesson/bar-chart";
+import DonutChart from "./components/lesson/donut-chart";
+import PieChart from "./components/lesson/pie-chart";
+import LineChart from "./components/lesson/line-chart";
+import RadarChart from "./components/lesson/radar-chart";
+import BoxPlot from "./components/lesson/box-plot";
+import StackedBarChart from "./components/lesson/stacked-bar-chart";
+import Treemap from "./components/lesson/treemap";
+import Heatmap from "./components/lesson/heatmap";
+import NetworkGraph from "./components/lesson/network-graph";
+import StreamGraph from "./components/lesson/stream-graph";
 
 export default function App() {
   return (
     <div>
-      <nav style={{ padding: "1rem", display: "flex", gap: "1rem" }}>
-        <Link to="/lesson-01">Lesson 01</Link>
-        <Link to="/lesson-02">Lesson 02</Link>
-        <Link to="/lesson-03">Lesson 03</Link>
-        <Link to="/lesson-04">Lesson 04</Link>
-        <Link to="/lesson-05">Lesson 05</Link>
-        <Link to="/lesson-06">Lesson 06</Link>
-        <Link to="/lesson-07">Lesson 07</Link>
+      <nav style={{ padding: "1rem", display: "flex", gap: "1rem", flexWrap: "wrap" }}>
+        <Link to="/svg-basics">SVG Basics</Link>
+        <Link to="/d3-dom-manipulation">D3 DOM</Link>
+        <Link to="/scatter-plot">Scatter Plot</Link>
+        <Link to="/bar-chart">Bar Chart</Link>
+        <Link to="/donut-chart">Donut Chart</Link>
+        <Link to="/pie-chart">Pie Chart</Link>
+        <Link to="/line-chart">Line Chart</Link>
+        <Link to="/radar-chart">Radar Chart</Link>
+        <Link to="/box-plot">Box Plot</Link>
+        <Link to="/stacked-bar-chart">Stacked Bar</Link>
+        <Link to="/treemap">Treemap</Link>
+        <Link to="/heatmap">Heatmap</Link>
+        <Link to="/network-graph">Network Graph</Link>
+        <Link to="/stream-graph">Stream Graph</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Lesson04 />} />
-        <Route path="/lesson-01" element={<Lesson01 />} />
-        <Route path="/lesson-02" element={<Lesson02 />} />
-        <Route path="/lesson-03" element={<Lesson03 />} />
-        <Route path="/lesson-04" element={<Lesson04 />} />
-        <Route path="/lesson-05" element={<Lesson05 />} />
-        <Route path="/lesson-06" element={<Lesson06 />} />
-        <Route path="/lesson-07" element={<Lesson07 />} />
+        <Route path="/" element={<BarChart />} />
+        <Route path="/svg-basics" element={<SvgBasics />} />
+        <Route path="/d3-dom-manipulation" element={<D3DomManipulation />} />
+        <Route path="/scatter-plot" element={<ScatterPlot />} />
+        <Route path="/bar-chart" element={<BarChart />} />
+        <Route path="/donut-chart" element={<DonutChart />} />
+        <Route path="/pie-chart" element={<PieChart />} />
+        <Route path="/line-chart" element={<LineChart />} />
+        <Route path="/radar-chart" element={<RadarChart />} />
+        <Route path="/box-plot" element={<BoxPlot />} />
+        <Route path="/stacked-bar-chart" element={<StackedBarChart />} />
+        <Route path="/treemap" element={<Treemap />} />
+        <Route path="/heatmap" element={<Heatmap />} />
+        <Route path="/network-graph" element={<NetworkGraph />} />
+        <Route path="/stream-graph" element={<StreamGraph />} />
       </Routes>
     </div>
   );
